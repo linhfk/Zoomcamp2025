0a218313f7cbc789e8e7a273531692120ce47211
linhfk <linhfk@gmail.com>
2025-04-07 22:41:12 -0400

    Update spark _command.py

--- Global Product Inventory/mage code/spark _command.py
+++ Global Product Inventory/mage code/spark _command.py
@@ -4,5 +4,5 @@ gcloud dataproc jobs submit pyspark \
     gs://global_inventory_data/clean_data_code.py \
     --\
         --input_data=gs://global_inventory_data/clean_data/raw/ \
-        --output_report=gs://global_inventory_data/clean_data/spark_submit/report \
-        --output_data=gs://global_inventory_data/clean_data/spark_submit/pq
+        --output_report=gs://global_inventory_data/clean_data/spark_submit/report \  #load transformed data to biquery
+        --output_data=gs://global_inventory_data/clean_data/spark_submit/pq  #load raw data to biquery
