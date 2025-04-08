d9cbe75b5ba53a223c264bb1e372881fe6f6c038
linhfk <linhfk@gmail.com>
2025-04-07 23:36:35 -0400

    Delete Global Product Inventory/mage code/spark _command.py

--- Global Product Inventory/mage code/spark _command.py
+++
@@ -1,8 +0,0 @@
-gcloud dataproc jobs submit pyspark \
-    --cluster=cluster-7c18 \
-    --region=us-east1 \
-    gs://global_inventory_data/clean_data_code.py \
-    --\
-        --input_data=gs://global_inventory_data/clean_data/raw/ \
-        --output_report=gs://global_inventory_data/clean_data/spark_submit/report \  #load transformed data to biquery
-        --output_data=gs://global_inventory_data/clean_data/spark_submit/pq  #load raw data to biquery
